119f6782ad9bb0b4fe74e987a94d0cc7876e1e2d
shaman_ando7 <andranikdashyan@mail.ru>
2022-08-10 17:30:48 +0400

    add Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Первое число");
+
+string? NumberString1 = Console.ReadLine ();
+int number1 = int.Parse (NumberString1!);
+
+Console.WriteLine ("Второе число");
+
+string? NumberString2 = Console.ReadLine ();
+int number2 = int.Parse (NumberString2!);
+int x = number2%number1;
+
+if (x==0)
+{
+Console.WriteLine ("Число кратно");
+}
+else
+{
+Console.WriteLine ("Число не кратно, остаток " + x);
+}
+
