bc83fe3cebee2ecfcec43357901605ea27eac3ec
Ваше имя <you@example.comgit>
2022-08-10 17:13:58 +0400

    add Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+Random random = new Random();
+
+int randomNumber = random.Next(100, 1000);
+
+Console.WriteLine(randomNumber);
+
+int firstnumber = randomNumber / 100; // Получаем первую цифру
+
+int secondnumber = randomNumber % 10; // Получаем последнюю
+
+int thirdnumber = firstnumber * 10; // Превращем первое число в двухзначное
+
+int finish =  thirdnumber + secondnumber; // Складываем двухзначное и последнее
+
+Console.WriteLine(finish);
